5919b22d9414e279084dbe414384bf7d6cf5674a
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2026-01-29 23:55:36 +0530

    Update BookManage.tsx

--- src/components/BookManage.tsx
+++ src/components/BookManage.tsx
@@ -10,6 +10,7 @@ interface Book {
 }
 
 
+
 const BookManagementApp: React.FC = () => {
   const [books, setBooks] = useState<Book[]>([]);
   const [editingIndex, setEditingIndex] = useState<number | null>(null);
