54ac77ca3533edd28b975d4503095ab05272e4d3
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2026-01-29 23:55:54 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import BookManagementApp from "@/components/BookManage";
 
 
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
